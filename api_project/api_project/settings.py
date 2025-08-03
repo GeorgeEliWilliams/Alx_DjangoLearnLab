2c5cb0bd385e5880cfadf942fe783c0bca129284
George Williams <williamsgeorge950@gmail.com>
2025-08-03 21:10:52 +0000

    api restructure

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j8ab_^w=jo*l0gyxjepekbv@as_w%4tfh9as$+f*c7iu!a*q^k'
+SECRET_KEY = 'django-insecure-5kgv!br6ho7%@9-p&e%bqc2teb^=6hft^#$cy=*mpw$wi-gl)@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
