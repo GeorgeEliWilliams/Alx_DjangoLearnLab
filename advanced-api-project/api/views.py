b59c28756f34af0be6c61313d9b8277f3c1cdfe5
George Williams <williamsgeorge950@gmail.com>
2025-08-10 22:35:29 +0000

    advanced api 01

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# ------------------------------
+# LIST + CREATE VIEW
+# ------------------------------
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    GET: List all books
+    POST: Create a new book (authenticated users only)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET: Retrieve a single book
+    PUT/PATCH: Update a book (authenticated users only)
+    DELETE: Delete a book (authenticated users only)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
